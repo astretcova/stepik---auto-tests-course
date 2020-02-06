844e6945279d8e161c5f830452805c59f4162dc1
astretcova <60731308+astretcova@users.noreply.github.com>
2020-02-06 12:45:16 +0300

    Задание: загрузка файла
    Открыть страницу http://suninjuly.github.io/file_input.html
    Заполнить текстовые поля: имя, фамилия, email
    Загрузить файл. Файл должен иметь расширение .txt и может быть пустым
    Нажать кнопку "Submit"

---
+++ 2.2_8.py
@@ -0,0 +1,29 @@
+import os
+from selenium import webdriver
+import time
+
+try:
+    link = " http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    input1 = browser.find_element_by_xpath('/html/body/div/form/div/input[1]')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_xpath("/html/body/div/form/div/input[2]")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_xpath('/html/body/div/form/div/input[3]')
+    input3.send_keys("email")
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла
+    file_path = os.path.join(current_dir, 'file.txt')           # добавляем к этому пути имя файла
+    button = browser.find_element_by_xpath('//*[@id="file"]')
+    #button.click()
+    button.send_keys(file_path)
+    button = browser.find_element_by_xpath('/html/body/div/form/button')
+    button.click()
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
